6e3c97a80c533a9238d2b31711124af3661e6b8b
demaxio <demaxio@mail.ru>
2019-06-25 21:43:59 +0400

    build

---
+++ precache-manifest.1c0bd897b03a74fa5ab624dcf2b098a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64502dd83c03957fc969578553707bc",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "84111de276900417e1c8",
+    "url": "/weather-app/static/css/main.2680dfa7.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/weather-app/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "84111de276900417e1c8",
+    "url": "/weather-app/static/js/main.47c0bd26.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  }
+]);
