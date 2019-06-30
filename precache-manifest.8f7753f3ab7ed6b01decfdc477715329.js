239a15489548b919685026bd9c607dacc616bd47
demaxio <45129571+demaxio@users.noreply.github.com>
2019-06-30 16:24:34 +0400

    Add files via upload

---
+++ precache-manifest.8f7753f3ab7ed6b01decfdc477715329.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f7b8acf714e56f8b4714b6214c8d64a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86cfabef61c14c1d26b7",
+    "url": "/static/css/main.a125df78.chunk.css"
+  },
+  {
+    "revision": "0862f05071723e50da14",
+    "url": "/static/js/2.0c694886.chunk.js"
+  },
+  {
+    "revision": "86cfabef61c14c1d26b7",
+    "url": "/static/js/main.c2612415.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cd7a2f14b2556630b0fe22adab32df67",
+    "url": "/static/media/wi-cloud.cd7a2f14.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Welcome to your Workbox-powered service worker!
  *
@@ -15,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9afd384d66ec23a37db93a2459a971cb.js"
+  "/precache-manifest.8f7753f3ab7ed6b01decfdc477715329.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -38,44 +37,3 @@ workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/i
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
-=======
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
-
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-importScripts(
-  "/weather-app/precache-manifest.1c0bd897b03a74fa5ab624dcf2b098a3.js"
-);
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
-
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-app/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
-});
->>>>>>> 4f97a9f9267ef244c8e1e7fd1ab6d656c612e4cf
