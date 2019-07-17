6d2aed43006d0eb42ec9590a1f1a347e6b3e090a
demaxio <45129571+demaxio@users.noreply.github.com>
2019-07-18 00:09:02 +0400

    Add files via upload

---
+++ precache-manifest.cf34d102ef71f5a92f4e9111ea74fcf0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2257241edda1aa66601c9aa0759d3cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03eaf7ab3fe4231d0847",
+    "url": "/static/css/main.ff74a62d.chunk.css"
+  },
+  {
+    "revision": "96b279bf25b049492a5c",
+    "url": "/static/js/2.8a6893a1.chunk.js"
+  },
+  {
+    "revision": "03eaf7ab3fe4231d0847",
+    "url": "/static/js/main.35fd3c97.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cd7a2f14b2556630b0fe22adab32df67",
+    "url": "/static/media/wi-cloud.cd7a2f14.svg"
+  },
+  {
+    "revision": "6fb7caa73d7c143590505c617a4ebec9",
+    "url": "/static/media/wi-rain.6fb7caa7.svg"
+  },
+  {
+    "revision": "53057272a6fc903376bcf52c6edac56d",
+    "url": "/static/media/wi-sun.53057272.svg"
+  }
+]);
