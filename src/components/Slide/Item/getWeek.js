6ea131fbbe3c9db0a739888aebcc90609e3ff3f7
demaxio <45129571+demaxio@users.noreply.github.com>
2019-07-25 23:49:34 +0400

    Add files via upload

--- src/components/Slide/Item/getWeek.js
+++ src/components/Slide/Item/getWeek.js
@@ -1,11 +1,11 @@
 var number= new Number();
 var now = new Date();
-export let dayNames = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+export let dayNames = ['Суббота', 'Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница'];
 if (now.getDay() + number === dayNames.length) {
       dayNames = dayNames[0];
     }
     if (now.getDay() + number > dayNames.length) {
       let differ = ((now.getDay() + number) - dayNames.length) % 7;
 	dayNames = dayNames[differ];}
-export let days = ['20 июля','21 июля', '22 июля','23 июля','24 июля','25 июля','26 июля','27 июля','28 июля','29 июля','30 июля','31 июля' ]; 
+export let days = ['26 июля','27 июля', '28 июля','29 июля','30 июля','31 июля','1 августа','2 августа','3 августа','4 августа','5 августа','6 августа' ]; 
 export var now = new Date();
